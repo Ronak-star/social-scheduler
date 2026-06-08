05a8e7a7163143711d87bebd934887751c711ef1
Ronak Tailor <tailorronak6@gmail.com>
2026-06-08 19:01:04 +0530

    Update API base URL to production environment

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000"
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://client-jet-three-66.vercel.app/"
 })
 
 export default api;

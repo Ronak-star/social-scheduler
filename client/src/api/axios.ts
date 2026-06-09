aacbb54f9e1aa68e74e1bbe101810fd14e457639
Ronak Tailor <tailorronak6@gmail.com>
2026-06-09 11:36:28 +0530

    Update API base URL in axios configuration

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "https://client-jet-three-66.vercel.app/"
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://backend-1e2b.vercel.app/"
 })
 
 export default api;
